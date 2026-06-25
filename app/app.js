eed55cd0a42c7375a47b3136b7b28f0abbd9fe9a
Carlos A. Caceres <carlos_acaceres@live.com>
2026-06-24 22:12:41 -0300

    Botones funcionales y persistencia

--- app/app.js
+++ app/app.js
@@ -3,12 +3,50 @@ function iniciarJornada() {
     const ahora = new Date();
 
     const fecha = ahora.toLocaleDateString();
-
     const hora = ahora.toLocaleTimeString();
+
+    const datos = {
+        fecha,
+        hora
+    };
+
+    localStorage.setItem(
+        "jornada",
+        JSON.stringify(datos)
+    );
+
+    mostrarJornada();
+}
+
+function mostrarJornada() {
+
+    const jornada = JSON.parse(
+        localStorage.getItem("jornada")
+    );
+
+    if (!jornada) return;
 
     document.getElementById("estadoJornada").innerHTML = `
         <h3>✅ Jornada iniciada</h3>
-        <p>Fecha: ${fecha}</p>
-        <p>Hora: ${hora}</p>
+        <p>Fecha: ${jornada.fecha}</p>
+        <p>Hora: ${jornada.hora}</p>
     `;
 }
+
+function recorrido() {
+    alert("Módulo Recorrido en construcción");
+}
+
+function cobros() {
+    alert("Módulo Cobros en construcción");
+}
+
+function vehiculo() {
+    alert("Módulo Vehículo en construcción");
+}
+
+function reportes() {
+    alert("Módulo Reportes en construcción");
+}
+
+mostrarJornada();
